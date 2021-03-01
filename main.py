eee225428f274c11a44a36ca9bfafa12b6874314
Mary <31658999+Mary230@users.noreply.github.com>
2021-03-01 12:21:56 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import urllib.request
 num = 1
 urls_f = open('urls.txt', 'r')
+index = open('index.txt', 'w')
 for line in urls_f:
     url = line
     html = ""
     sock = urllib.request.urlopen(url)
     html = sock.read()
     html = html.decode()
-    num += 1
     f = open('htmls/'+ 'file_' + str(num) +'.txt', 'w')
+    index.write('file_' + str(num) + '  ' + line + ' \n')
+    num += 1
     f.write(html)
     f.close()
     sock.close()
